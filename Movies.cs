bb5f7dab4d129e72aa1fea63262e018e0aed66f6
Kalata-22 <80474953+Kalata-22@users.noreply.github.com>
2021-03-21 11:09:22 +0200

    Add files via upload

---
+++ Movies.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_Operations.Models
+{
+    public partial class Movies
+    {
+        public Movies()
+        {
+            Movies = new HashSet<Movies>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string YearOfCreation { get; set; }
+        public string Genre { get; set; }
+        public string Producer { get; set; }
+        public int Cast { get; set; }
+        public DateTimeOffset Created { get; set; }
+
+        public virtual ICollection<Movies> Movies { get; set; }
+    }
+} 
---
+++ MyDbContext.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUD_Operations.Models
+    
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Admins> Admins { get; set; }
+        public virtual DbSet<Movies> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=12345;database=hospital_db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Admins>(entity =>
+            {
+                entity.ToTable("admins");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Created)
+                    .HasColumnName("created")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasColumnName("phone")
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Movies>(entity =>
+            {
+                entity.ToTable("movies");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.YearOfCreation)
+                    .IsRequired()
+                    .HasColumnName("yearofcreation")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Genre)
+                    .HasColumnName("genre")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Producer)
+                    .IsRequired()
+                    .HasColumnName("producer")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cast)
+                    .IsRequired()
+                    .HasColumnName("cast")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+           
+
+           
+            
+        }
+    }
+}
